5bc067b1a2d3930fcf7d7cd7515777c4a061bbfb
Cristian Díaz <cristian@210x7.io>
2025-11-21 19:44:02 +0100

    smith-validation 1.0.9

--- smith-validation.rb
+++ smith-validation.rb
@@ -1,15 +1,15 @@
 class SmithValidation < Formula
   desc "TCA architectural validation engine (Rules 1.1-1.5) for Smith Tools"
   homepage "https://github.com/Smith-Tools/smith-validation"
-  url "https://github.com/Smith-Tools/smith-validation/archive/refs/tags/v1.0.8.tar.gz"
-  sha256 "4176fc0c1585abfad5902d9ef1784d32e9f652ab40efd047f38d928a39dcf10a"
+  url "https://github.com/Smith-Tools/smith-validation/archive/refs/tags/v1.0.9.tar.gz"
+  sha256 "13901cda30c33c32744e2b49ddf59dc3ab57f63a9f7b95ef3128aa5eac165d61"
   license "MIT"
   head "https://github.com/Smith-Tools/smith-validation.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/Smith-Tools/homebrew-smith/releases/download/smith-validation-1.0.8"
-    rebuild 1
-    sha256 cellar: :any, arm64_tahoe: "da33a92b93b7e859927aac983a1e0d5bf37c67e6351a1d5149a798a6ba8569e7"
+    root_url "https://github.com/Smith-Tools/homebrew-smith/releases/download/smith-validation-1.0.9"
+    rebuild 0
+    sha256 cellar: :any, arm64_tahoe: ""
   end
 
   depends_on xcode: ["15.0", :build]
